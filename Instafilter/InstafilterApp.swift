69404c2018825f4eb5ef82eb18f094c8f1a58cd5
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-05 06:29:39 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Adam Elfsborg on 2024-08-05.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
