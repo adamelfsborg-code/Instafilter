296319a8fb3fbabd50dbd91d31e58bc64a02ed13
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-07 06:33:44 +0200

    feat: import pictures from photo app

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,16 +4,33 @@
 //
 //  Created by Adam Elfsborg on 2024-08-05.
 //
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
     var body: some View {
-        ContentUnavailableView {
-            Label("No Snippets", systemImage: "swift")
-        } description: {
-            Text("You don't have any snippets saved yet.")
-        } actions: {
-            Button("Add your first snippet") { }.buttonStyle(.borderedProminent)
+        VStack {
+            PhotosPicker(selection: $pickerItems, maxSelectionCount: 5, matching: .any(of: [.images, .not(.screenshots)])) {
+                Label("Select a picture", systemImage: "photo")
+            }
+            
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i].resizable().scaledToFit()
+                }
+            }
+        }
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                for item in pickerItems {
+                    if let loadedImage =  try await item.loadTransferable(type: Image.self) {
+                        selectedImages.insert(loadedImage, at: 0)
+                    }
+                }
+            }
         }
     }
 }
