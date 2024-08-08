abb1042c2ae3605df81f0062e4e8b8ddf23047bf
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-08 06:18:40 +0200

    feat: core app

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,20 +4,75 @@
 //
 //  Created by Adam Elfsborg on 2024-08-05.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var selectedImage: PhotosPickerItem?
+    @State private var filterIntensity = 0.5
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
-        let tuscany = Image(.tuscany)
-        VStack {
-            ShareLink(item: URL(string: "https://hackingwithswift.com")!, subject: Text("Learn Swift"), message: Text("Check out the 100 days of Swift challenge")) {
-                Label("Spread the word", systemImage: "swift")
+        NavigationStack {
+            VStack {
+                Spacer()
                 
+                PhotosPicker(selection: $selectedImage) {
+                    if let processedImage {
+                        processedImage.resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to add picture"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedImage, loadImage)
                 
-                ShareLink(item: tuscany, preview: SharePreview("Tuscany wallpaper", image: tuscany))
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                
+                HStack {
+                    Button("Change filter", action: changeFilter)
+                    
+                    Spacer()
+                }
             }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
+    }
+    
+    func changeFilter() { }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedImage?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
